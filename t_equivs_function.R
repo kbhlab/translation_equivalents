07e51ef252d8c8063808f77d6cd25e89b21bdeda
Hilary Rose <hilary.killam@gmail.com>
2019-08-27 17:41:49 -0400

    Update t_equivs_function.R

--- t_equivs_function.R
+++ t_equivs_function.R
@@ -3,7 +3,7 @@
 t_equivs <- function(en_data, fr_data, output = "by_baby") {
 
   case_when(
-    ncol(en_data) != 555 | ncol(en_data) != 858 | ncol(fr_data) != 572 | ncol(fr_data) != 815 ~ stop("Warning: your input data must be in the format of 'CSV/ALL' from WebCDI!"),
+    ncol(en_data) != 555 | ncol(en_data) != 858 & ncol(fr_data) != 572 | ncol(fr_data) != 815 ~ stop("Warning: your input data must be in the format of 'CSV/ALL' from WebCDI!"),
     TRUE ~ warning("Data format okay")
     )
 
